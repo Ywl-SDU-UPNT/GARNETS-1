2f61cfddcb9568e9ad338f4173577d76e6f0f990
YWL-SDU <202221158@mail.sdu.edu.cn>
2023-04-10 23:29:59 +0800

    delete some code

--- Lib/Grid_Lib/RefinedTravelTime.m
+++
@@ -1,4 +0,0 @@
-function [Time,Sig_y] = RefinedTravelTime(Horizontal,GridData,WindowNum,Order)
-WindowData = SearchWindow(Horizontal,GridData,WindowNum);
-[Time,Sig_y] = PFGrid2IncidentAngle(Horizontal,WindowData,Order);
-end
